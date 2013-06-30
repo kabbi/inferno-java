627545051e30c5f717d7c48dfc73f4fe8d64f67f
Dmitry Kabak (userdima <Dmitry Kabak (userdima@gmail.com)>
2013-06-30 22:27:34 +0300

    Some environment fixes.

--- setupenv.sh
+++ setupenv.sh
@@ -2,3 +2,10 @@ export INFERNO_ROOT=$(pwd)
 export EMU="-r$INFERNO_ROOT -g1024x600"
 export PATH=$INFERNO_ROOT/MacOSX/386/bin:$PATH
 alias emug="rlwrap -a blabla emu-g"
+alias mkemu="mk install"
+alias mkemug="mk install CONF=emu-g"
+alias mkjava="cd java; mk install; cd .."
+
+function ifind {
+    find $1 | xargs grep -I $2
+}
