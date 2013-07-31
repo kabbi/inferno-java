464f58bb2b4787e3f2b5e51af8b30a10c34258d4
Dmitry Kabak (userdima <Dmitry Kabak (userdima@gmail.com)>
2013-07-31 11:09:42 +0300

    Updated native FileOutputStream to conform the modern java class.

--- java/pkg/java/io/FileOutputStream_L.m
+++ java/pkg/java/io/FileOutputStream_L.m
@@ -9,10 +9,9 @@ FileOutputStream_L : module
     };
 
     init : fn( jni_p : JNI );
-    open_rString_V : fn( this : ref FileOutputStream_obj, p0 : JString);
-    openAppend_rString_V : fn( this : ref FileOutputStream_obj, p0 : JString);
-    write_I_V : fn( this : ref FileOutputStream_obj, p0 : int);
-    writeBytes_aB_I_I_V : fn( this : ref FileOutputStream_obj, p0 : JArrayB,p1 : int,p2 : int);
+    open_rString_Z_V : fn( this : ref FileOutputStream_obj, p0 : JString, p1 : int);
+    write_I_Z_V : fn( this : ref FileOutputStream_obj, p0 : int, p1 : int);
+    writeBytes_aB_I_I_Z_V : fn( this : ref FileOutputStream_obj, p0 : JArrayB,p1 : int,p2 : int, p3 : int);
     close0_V : fn( this : ref FileOutputStream_obj);
     initIDs_V: fn( );
 
